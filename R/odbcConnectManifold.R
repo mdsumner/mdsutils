a369dd70f5e3f4b9127ba1cc7a605047e6879178
mdsumner <mdsumner@gmail.com>
2014-09-25 11:57:01 +1000

    functions to do the actual read
    These are pretty hacky but could be improved, the worst is converting the CRS

--- R/odbcConnectManifold.R
+++ R/odbcConnectManifold.R
@@ -53,3 +53,41 @@ odbcConnectManifold <- function (map.file)
 
 }
 
+
+
+
+manifoldCRS <- function(connection, componentname) {
+    sqlQuery(connection, sprintf('SELECT TOP 1 CoordSysToWKT(CoordSys("%s" AS COMPONENT)) AS [CRS] FROM [%s]', componentname, componentname), stringsAsFactors = FALSE)$CRS
+}
+
+wktCRS2proj4 <- function(CRS) {
+    require(rgdal)
+    dsn <- tempdir()
+    f <- basename(tempfile())
+    writeOGR(SpatialPointsDataFrame(SpatialPoints(cbind(1, 1)), data.frame(x = 1)), dsn, f, "ESRI Shapefile", overwrite = TRUE)
+    writeLines(CRS, paste(file.path(dsn, f), ".prj", sep = ""))
+    proj4 <- proj4string(readOGR(dsn, f, verbose = FALSE))
+    proj4
+
+}
+
+
+wkt2Spatial <- function(x, id = NULL, p4s = NULL, data = data.frame(x = 1:length(x), row.names = id), ...) {
+    ##res <- vector("list", length(x))
+    require(rgeos)
+    require(maptools)
+    if (is.null(id)) id <- as.character(seq_along(x))
+    for (i in seq_along(x)) {
+        a1 <- readWKT(x[i], id = id[i], p4s = p4s)
+        if (i == 1) {
+            res <- a1
+        } else {
+            res <- spRbind(res, a1)
+        }
+    }
+    if (is(res, "SpatialPolygons")) {
+        res <- SpatialPolygonsDataFrame(res, data, ...)
+    }
+
+    res
+}
